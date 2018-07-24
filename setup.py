413bb3b94fea8d4d0c0ef9dab92d11ae89f4cded
CDN MacbookPro13 <chandan.nath@gmail.com>
2018-07-24 01:11:20 +0100

    changes for setup

--- setup.py
+++ setup.py
@@ -1,21 +1,12 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="drypy_airflow",
     version="0.0.1",
     author="dhan16",
     author_email="chandan.nath@gmail.com",
     description="Airflow extensions",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/dhan16/drypy-airflow",
     packages=setuptools.find_packages(),
-    classifiers=(
-        "Programming Language :: Python :: 3",
-        "License :: Apache License :: Version 2.0, January 2004",
-        "Operating System :: OS Independent",
-    ),
+    license="Apache License Version 2.0, January 2004",
 )
