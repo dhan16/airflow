2a72c8e20a1b4c7ce8f7d2e8f6bf25b323915290
CDN MacbookPro13 <chandan.nath@gmail.com>
2018-07-24 01:36:07 +0100

    for readme

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="drypy_airflow",
-    version="0.0.1",
+    version="0.0.3",
     author="dhan16",
     author_email="chandan.nath@gmail.com",
     description="Airflow extensions",
